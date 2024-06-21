e705b0ed72ecfb4c53ba0a8cba34b8884072946a
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-06-21 14:21:31 +1200

    Update test_sim.c
    Added slightly better documentation.

--- test_sim.c
+++ test_sim.c
@@ -5,6 +5,17 @@
 #include <time.h>
 #include "simulate.c"
 
+/**
+ * Prints the contents of the board as a 2D boolean array.
+ *
+ * @param board A pointer to the board.
+ * @param m The number of rows in the board.
+ * @param n The number of columns in the board.
+ *
+ * @return void
+ *
+ * @throws None
+ */
 void print_board(bool** board, int m, int n) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -14,6 +25,17 @@ void print_board(bool** board, int m, int n) {
     }
 }
 
+
+/**
+ * Runs the main function for testing the game of life simulation.
+ *
+ * @param argc the number of command-line arguments
+ * @param argv an array of strings containing the command-line arguments
+ *
+ * @return 0 if the function completes successfully
+ *
+ * @throws None
+ */
 int main(int argc, char** argv) {
     int m = 10;
     int n = 10;
