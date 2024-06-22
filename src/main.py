20755d9cb34cf4aabb4d42a66abe54930d0ea782
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-06-22 16:01:13 +1200

    Create main.py
    Added the main file - this program is far from complete though.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""A simple implementation of Conway's game of life."""
+
+from modules.ui.create_ui import ConwaysGameOfLife
+
+
+def main() -> None:
+    """The main function."""
+    ConwaysGameOfLife().run()
+
+
+if __name__ == '__main__':
+    main()
