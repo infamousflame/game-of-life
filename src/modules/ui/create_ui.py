fe6bdf5ac2cda809c04dbd870302a14de833b68b
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-06-25 16:13:34 +1200

    Update create_ui.py
    Ditched RootLayout class completely, instead just returning the built KvLang.

--- src/modules/ui/create_ui.py
+++ src/modules/ui/create_ui.py
@@ -4,23 +4,14 @@ __all__ = ['MainUI']
 
 from kivy.app import App
 from kivy.lang.builder import Builder
-from kivy.uix.boxlayout import BoxLayout
 
 from modules.sim.run import board
-
-with open('./assets/ui/root_layout.kv', 'rt') as f:
-    Builder.load_string(f.read())
-
-
-class RootLayout(BoxLayout):
-    """The root layout."""
 
 
 class MainUI(App):
     """The app class."""
 
-    def build(self) -> RootLayout:
+    def build(self):
         """Build the user interface."""
-        self.window: RootLayout = RootLayout()
-        self.title = "Conway's Game of Life"
-        return self.window
+        with open('./assets/ui/root_layout.kv', 'rt') as f:
+            return Builder.load_string(f.read())
