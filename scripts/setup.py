66150cbde0ee6776d471a0888fea7bcdd3047f22
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-07-15 13:10:32 +1200

    Update setup.py
    Added check for virtual environment to setup script.

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,11 @@
 """Compile the C library."""
 
+from os import environ
 from setuptools import setup, Extension
+
+if 'VIRTUAL_ENV' not in environ:
+    print('Please run this script within a virtual environment.')
+    exit(1)
 
 module = Extension('simulate', sources = ['./src/modules/simulate/python_wrappers.c'])
 
